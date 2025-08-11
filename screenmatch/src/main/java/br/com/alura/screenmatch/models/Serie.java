9d8d0d3ad162617d2f1e8d83a0fdaa525c302b95
FooX-1999 <leao741fooda@gmail.com>
2025-08-10 23:01:03 -0300

    Dia 10/08 arrumado as senhas e conctou no postgree

--- screenmatch/src/main/java/br/com/alura/screenmatch/models/Serie.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/models/Serie.java
@@ -1,16 +1,36 @@
 package br.com.alura.screenmatch.models;
 
 
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "Series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+//    @Column(name = "nomeDaSerie")
+    @Column(unique = true)
     private String titulo;
     private int totalTemporadas;
     private double avaliacao;
+    @Transient
+    private List<Episodio> episodios = new ArrayList<>();
+
+
+
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String autores;
     private String poster;
     private String sinopse;
+
+    public Serie(){}
 
     public Serie(DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
@@ -20,6 +40,90 @@ public class Serie {
         this.autores = dadosSerie.autores();
         this.poster = dadosSerie.poster();
         this.sinopse = dadosSerie.sinopse();
+//        this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse().trim());
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Episodio> getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        this.episodios = episodios;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public void setTotalTemporadas(int totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getAutores() {
+        return autores;
+    }
+
+    public void setAutores(String autores) {
+        this.autores = autores;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    @Override
+    public String toString() {
+        return
+                " genero= '" + genero + '\'' +
+                " titulo= '" + titulo + '\'' +
+                ", totalTemporadas= '" + totalTemporadas +
+                ", avaliacao= '" + avaliacao + '\'' +
+                ", autores= '" + autores + '\'' +
+                ", poster= '" + poster + '\'' +
+                ", sinopse= '" + sinopse + '\'';
+    }
 }
--- screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -2,12 +2,17 @@ package br.com.alura.screenmatch.principal;
 
 import br.com.alura.screenmatch.models.DadosSerie;
 import br.com.alura.screenmatch.models.DadosTemporada;
+import br.com.alura.screenmatch.models.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
@@ -18,6 +23,14 @@ public class Principal {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=9797c560";
     private List<DadosSerie> dadosSeries = new ArrayList<>();
+
+
+    private SerieRepository repositorio;
+
+    public Principal(SerieRepository repositorio) {
+        this.repositorio = repositorio;
+    }
+
     public void exibeMenu() {
         var opcao = -1;
         while (opcao != 0) {
@@ -55,7 +68,10 @@ public class Principal {
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
-        dadosSeries.add(dados);
+        Serie serie = new Serie(dados);
+//        dadosSeries.add(dados);
+          repositorio.save(serie);
+
         System.out.println(dados);
     }
 
@@ -80,7 +96,12 @@ public class Principal {
     }
 
     private void listarSerieBuscada() {
-        dadosSeries.forEach(System.out::println);
+
+        List<Serie> series = repositorio.findAll();
+        series.stream()
+                .sorted(Comparator.comparing(Serie::getGenero))
+                .forEach(System.out::println);
+
     }
 }
 
